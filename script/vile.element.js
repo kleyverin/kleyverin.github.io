9171c7372d5217a27ed17789090a830e65a0a213
Dea Alverina <dea.alverina@ti.ukdw.ac.id>
2016-05-28 18:31:14 +0700

    no message

--- script/vile.element.js
+++ script/vile.element.js
@@ -10,15 +10,15 @@
 							content: function(element){
 								var style = "<style>"
 								style+= ' div{display:inline-block; padding: 3px 2px;}'
-								style+= ' .l{ -webkit-animation: jump 1s ease-out 1s infinite normal; animation: jump 1s ease-out 1s infinite normal; animation-delay: 0.1s;}'
-								style+= ' .o{ -webkit-animation: jump 1s ease-out 1s infinite normal; animation: jump 1s ease-out 1s infinite normal; animation-delay: 0.15s;}'
-								style+= ' .a{ -webkit-animation: jump 1s ease-out 1s infinite normal; animation: jump 1s ease-out 1s infinite normal; animation-delay: 0.2s;}'
-								style+= ' .d{ -webkit-animation: jump 1s ease-out 1s infinite normal; animation: jump 1s ease-out 1s infinite normal; animation-delay: 0.25s;}'
-								style+= ' .i{ -webkit-animation: jump 1s ease-out 1s infinite normal; animation: jump 1s ease-out 1s infinite normal; animation-delay: 0.3s;}'
-								style+= ' .n{ -webkit-animation: jump 1s ease-out 1s infinite normal; animation: jump 1s ease-out 1s infinite normal; animation-delay: 0.35s;}'
-								style+= ' .g{ -webkit-animation: jump 1s ease-out 1s infinite normal; animation: jump 1s ease-out 1s infinite normal; animation-delay: 0.4s;}'
-								style+= ' @-webkit-keyframes jump {25%{-webkit-transform: translateY(0);transform: translateY(0);}40%{-webkit-transform: translateY(-5px);transform: translateY(-5px);}50%{-webkit-transform: translateY(0);transform: translateY(0);}}'
-								style+= ' @keyframes jump {30%{transform: translateY(0);}40%{transform: translateY(-5px);}50%{transform: translateY(0);}}'
+								style+= ' .l{ -webkit-animation: jump 1.5s ease-out 1.5s infinite normal; animation: jump 1.5s ease-out 1s infinite normal; animation-delay: 0.1s;}'
+								style+= ' .o{ -webkit-animation: jump 1.5s ease-out 1.5s infinite normal; animation: jump 1.5s ease-out 1s infinite normal; animation-delay: 0.15s;}'
+								style+= ' .a{ -webkit-animation: jump 1.5s ease-out 1.5s infinite normal; animation: jump 1.5s ease-out 1s infinite normal; animation-delay: 0.2s;}'
+								style+= ' .d{ -webkit-animation: jump 1.5s ease-out 1s infinite normal; animation: jump 1.5s ease-out 1s infinite normal; animation-delay: 0.25s;}'
+								style+= ' .i{ -webkit-animation: jump 1.5s ease-out 1.5s infinite normal; animation: jump 1.5s ease-out 1s infinite normal; animation-delay: 0.3s;}'
+								style+= ' .n{ -webkit-animation: jump 1.5s ease-out 1.5s infinite normal; animation: jump 1.5s ease-out 1s infinite normal; animation-delay: 0.35s;}'
+								style+= ' .g{ -webkit-animation: jump 1.5s ease-out 1.5s infinite normal; animation: jump 1.5s ease-out 1s infinite normal; animation-delay: 0.4s;}'
+								style+= ' @-webkit-keyframes jump {25%{-webkit-transform: translateY(0);transform: translateY(0);}40%{-webkit-transform: translateY(-10px);transform: translateY(-10px);}50%{-webkit-transform: translateY(0);transform: translateY(0);}}'
+								style+= ' @keyframes jump {30%{transform: translateY(0);}40%{transform: translateY(-10px);}50%{transform: translateY(0);}}'
 								style+='</style>'
 								var content = ""
 								content+= "<div class=\"l\">L</div>"
