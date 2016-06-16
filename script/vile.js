2fc97cd643c92dd2f0c3e92d46726e0435647cc0
Alan Darmasaputra <alan.darmasaputra@ti.ukdw.ac.id>
2016-06-17 00:57:36 +0700

    Telepath cord

--- script/vile.js
+++ script/vile.js
@@ -676,5 +676,218 @@ var Vile = {
 				argsType: argsType
 			}
 		}
+	},
+	Telepath: {
+		list_master: function(){
+			return {
+						'INPUT': true,
+						'SELECT': true
+					}
+		},
+		transmit: function(value,element){
+			if(typeof element.filter === 'function'){
+				value = element.filter(value)
+			}
+			if(typeof value == 'boolean' || typeof value == 'number' || typeof value == 'string'){
+				if(element.attribute == null){
+					switch(element.element.nodeName.toUpperCase()){
+							case 'BUTTON'	: 
+							case 'METER'	: 
+							case 'PROGRESS'	: 
+							case 'TEXTAREA'	: 
+							case 'PARAM'	:
+							case 'SELECT'	: element.element.value = value; break 
+							case 'INPUT'	:
+								if(element.element.getAttribute('type')=='checkbox'
+								   || element.element.getAttribute('type')=='radio'){
+									element.element.checked = value
+								}else{
+									element.element.value = value; break 
+								}
+							case 'AREA'		:
+							case 'BASE'		:
+							case 'BR'		:
+							case 'COL'		:
+							case 'COMMAND'	:
+							case 'EMBED'	:
+							case 'HR'		:
+							case 'IMG'		:
+							case 'KEYGEN'	:
+							case 'LINK'		:
+							case 'META'		:
+							case 'SOURCE'	:
+							case 'TRACK'	:
+							case 'WBR'		: break;
+							default			: element.element.textContent = value; break;
+					}
+				}else{
+					element.element.setAttribute(element.attribute,value)
+				}
+			}
+		},
+		receive: function(element,cord){
+			if(cord.getCord().lockReceive){
+				return
+			}
+			cord.getCord().lockReceive = true
+			if(element.nodeName == 'INPUT'){
+				var type = element.getAttribute('type')
+				if(type=='radio' || type=='checkbox'){
+					cord.getCord().value = element.checked
+					cord.refresh(element)
+				}
+				else{
+					cord.getCord().value = element.value
+					cord.refresh(element)
+				}
+			}
+			else if(element.nodeName == 'SELECT'){
+				cord.getCord().value = element.value
+				cord.refresh(element)
+			}
+			cord.getCord().lockReceive = false
+		},
+		removeMaster: function(element){
+			element.onkeyup = null
+			element.onchange = null
+		},
+		bindMaster: function(element,cord){
+			console.log(cord)
+			element.onkeyup = null
+			element.onchange = null
+			if(element.nodeName == 'INPUT'){
+				var type = element.getAttribute('type')
+				if(type=='radio' || type=='checkbox'){
+					element.onchange = function(){
+						Vile.Telepath.receive(element,cord)
+					}
+				}
+				else{
+					if(cord.getCord().hardBound){
+						element.onkeyup = function(){
+							Vile.Telepath.receive(element,cord)
+						}
+						element.onchange = function(){
+							Vile.Telepath.receive(element,cord)
+						}
+					}
+					else{
+						element.onchange = function(){
+							Vile.Telepath.receive(element,cord)
+						}
+					}
+				}
+			}
+			else if(element.nodeName == 'SELECT'){
+				element.onchange = function(){
+					Vile.Telepath.receive(element,cord)
+					cord.getCord().value = element.value
+					cord.refresh()
+				}
+			}
+		},
+		searchCordElement: function(list,element){
+			for(var i = 0; i<list.length; i++){
+				if(list[i].element == element){
+					return i;
+				}
+			}
+		},
+		rebindAllMaster: function(cord){
+			var mE = cord.getCord().masterElement
+			for(var i = 0; i<mE.length; i++){
+				Vile.Telepath.bindMaster(mE[i],this)
+			}
+		},
+		quickbind: function(master,slave,hardBound=true){
+			var cord = new Vile.Telepath.Cord()
+			cord.setHardBound(hardBound)
+			cord.addMaster(master)
+			cord.addSlave(slave)
+			return cord
+		},
+		Cord: function(){
+			var cord = {
+				value: null,
+				slaveElement: [],
+				masterElement: [],
+				lockRefresh: false,
+				hardBound: false
+			};
+			var pub = {}
+			pub.getCord = function(){return cord}
+			pub.set = function(ref){cord.value = ref;pub.refresh()}
+			pub.setHardBound = function(bool){cord.hardBound = bool; Vile.Telepath.rebindAllMaster(this)}
+			pub.get = function(){return cord.value}
+			pub.addMaster = function(element){
+				if(Vile.Telepath.list_master()[element.nodeName]!=true){
+					throw new VileException('This element cannot be master')
+				}
+				if(element instanceof HTMLElement){
+					cord.masterElement.push({
+						element: element
+					})
+				}else{
+					throw VileException('Not an HTMLElement')
+				}
+				Vile.Telepath.transmit(cord.value,cord.masterElement[cord.masterElement.length-1])
+				Vile.Telepath.bindMaster(element,this)
+			}
+			pub.removeMaster = function(element){
+				cord.masterElement.splice(Vile.Telepath.searchCordElement(cord.masterElement,element),1)
+				Vile.removeMaster(element)
+			}
+			pub.addSlave = function(element,attribute){
+				if(element instanceof HTMLElement){
+					cord.slaveElement.push({
+						element: element,
+						attribute: attribute
+					})
+				}else{
+					throw VileException('Not an HTMLElement')
+				}
+				Vile.Telepath.transmit(cord.value,cord.slaveElement[cord.slaveElement.length-1])
+			}
+			pub.removeSlave = function(element){
+				cord.slaveElement.splice(Vile.Telepath.searchCordElement(cord.slaveElement,element),1)
+			}
+			pub.setSlaveFilter = function(element,filter){
+				if(typeof filter !== 'function'){
+					throw new VileException('Filter must be function that returns string which is an altered parameter')
+				}
+				var index = Vile.Telepath.searchCordElement(cord.slaveElement,element)
+				cord.slaveElement[index].filter = filter
+				Vile.Telepath.transmit(cord.value,cord.slaveElement[index])
+			}
+			pub.removeSlaveFilter = function(element){
+				cord.slaveElement[Vile.Telepath.searchCordElement(cord.slaveElement,element)].filter = null
+				Vile.Telepath.transmit(cord.value,element)
+			}
+			pub.setSlaveAttribute = function(element,attribute){
+				var index = Vile.Telepath.searchCordElement(cord.slaveElement,element)
+				cord.slaveElement[index].attribute = attribute
+				Vile.Telepath.transmit(cord.value,cord.slaveElement[index])
+			}
+			pub.removeSlaveAttribute = function(element){
+				cord.slaveElement[Vile.Telepath.searchCordElement(cord.slaveElement,element)].attribute = null
+				Vile.Telepath.transmit(cord.value,element)
+			}
+			pub.refresh = function(exception){
+				if(cord.lockRefresh){
+					return
+				}
+				cord.lockRefresh = true
+				for(var i = 0; i<cord.masterElement.length; i++){
+					if(cord.masterElement[i].element!=exception){
+						Vile.Telepath.transmit(cord.value,cord.masterElement[i])
+					}
+				}
+				for(var i = 0; i<cord.slaveElement.length; i++){
+					Vile.Telepath.transmit(cord.value,cord.slaveElement[i])
+				}
+				cord.lockRefresh = false
+			}
+			return pub
+		}
 	}
 }
