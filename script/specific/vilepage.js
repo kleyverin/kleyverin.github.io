efe80d914e5c32a6507cb1ebec57125e4d9b7c01
Alan Darmasaputra <alan.darmasaputra@ti.ukdw.ac.id>
2016-09-24 00:16:12 +0700

    Vile preparing

--- script/specific/vilepage.js
+++ script/specific/vilepage.js
@@ -1,6 +1,10 @@
 var page = Vile.initialize()
 window.addEventListener('load',function(){
 
-	document.title = "Vile.js | Kley & Verin";
+	setTimeout(function(){
+		document.title = "Vile.js | Kley & Verin"
+		$(page.app_header).removeClass('big')
+		$(page.loading_indicator).addClass('disappear')
+	},2000)
 
 },true)
